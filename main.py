9ff753f2c8d15f6cd65fb5711825fe43e699da2a
Mateusz Serek <mateuszserek90@gmail.com>
2025-06-26 01:16:26 +0200

    text input, not working os.rename function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import sys 
 import os
-from PyQt5.QtWidgets import QPushButton, QLabel, QVBoxLayout, QApplication, QFileDialog
+from PyQt5.QtWidgets import QPushButton, QLabel, QVBoxLayout, QApplication, QFileDialog, QLineEdit
 from PyQt5 import QtWidgets
 
 class File_name_editor_app(QtWidgets.QWidget):
@@ -8,22 +8,30 @@ class File_name_editor_app(QtWidgets.QWidget):
         super().__init__()
         self.configure_window()
 
-        layout = QtWidgets.QFormLayout()
+        layout = QVBoxLayout()
 
         self.label = QLabel("")
         self.select_directory_button = QPushButton("Wybierz folder")
+        self.input = QLineEdit()
 
         layout.addWidget(self.select_directory_button)
         layout.addWidget(self.label)
+        layout.addWidget(self.input)
+
         self.setLayout(layout)
 
         self.select_directory_button.clicked.connect(self.select_directory_action)
 
     def configure_window(self):
         self.setWindowTitle("app")
-        self.setMinimumSize(400, 400)
+        self.setMinimumSize(600, 400)
 
     def select_directory_action(self):
+        if self.input.text() == "":
+            self.label.setText("nazwa nie może być pusta")
+            return
+
+
         file_directory = str(QFileDialog.getExistingDirectory(self, "Wybierz odpowiedni folder"))
         self.label.setText(file_directory)
 
@@ -35,6 +43,11 @@ class File_name_editor_app(QtWidgets.QWidget):
 
         if not files:
             self.label.setText("Brak plików do zmiany")
+            return 
+        counter = 0
+        for file_name in files:
+            os.rename(os.path.abspath(file_name), f"{os.path.abspath(file_name)}{counter}")
+            counter += 1
             
 
 
